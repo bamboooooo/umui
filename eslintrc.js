7ffbefd58cce0ef386cd26fb7a40325da56ab25e
林伟 <lin.wei@ucsmy.com>
2017-07-10 15:50:51 +0800

    修改eslint校验规则

--- eslintrc.js
+++ eslintrc.js
@@ -55,7 +55,7 @@ module.exports = {
 		"no-control-regex": 2,
 		// 数组和对象键值对最后一个逗号， never参数：不能带末尾的逗号, always参数：必须带末尾的逗号，
 		// always-multiline：多行模式必须带逗号，单行模式不能带逗号
-		"comma-dangle": [1, "always-multiline"],
+		"comma-dangle": [1, "never"],
 		// 禁用 debugger
 		"no-debugger": 2,
 		// 禁止 function 定义中出现重名参数
