9a1fa47ce946bf49586f1c5bae20edd128410ad8
杜慧玲 <du.huiling@ucsmy.com>
2017-07-17 19:19:58 +0800

    添加float-button组件，固定在页面的某个位置，不会随页面滚动而滚动。 ISSUE: resolve #23

---
+++ src/base/float-button/index.js
@@ -0,0 +1,44 @@
+/**
+ * 创建人：DuHuiling
+ * 创建时间：2017/7/17
+ * 说明：漂浮按钮组件
+ */
+var classnames = require('classnames');
+var FloatButton = React.createClass({
+    getDefaultProps: function () {
+        return {
+
+        };
+    },
+    getInitialState: function () {
+        return {
+            hide: false
+        };
+    },
+    show: function () {
+        this.setState({
+            hide: false
+        });
+    },
+    hide: function () {
+        this.setState({
+            hide: true
+        });
+    },
+    render: function () {
+        var _classname = classnames('ucs-float-button', this.props.className, {'ucs-hide': this.state.hide});
+        var _textClass = classnames('ucs-float-button-text',{'ucs-float-only-text':!this.props.img});
+        var _imgClass = classnames('ucs-float-button-img',{'ucs-float-only-img':!this.props.text});
+        return (
+            <div className={_classname} id={this.props.id} onClick = {this.props.onClick}>
+                {this.props.img
+                    ? <p className={_imgClass}><img src={this.props.img}/></p> : ''
+                }
+                {this.props.text
+                    ? <p className={_textClass}>{this.props.text}</p> : ''
+                }
+            </div>
+        );
+    }
+});
+module.exports = FloatButton;
